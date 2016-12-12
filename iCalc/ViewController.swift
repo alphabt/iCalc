2a6ef2438734d7a03aa845b83f8c835f67253b5e
Brian Tai <alphabt@gmail.com>
2016-12-12 23:50:54 +0800

    Init iCalc

---
+++ iCalc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iCalc
+//
+//  Created by Brian Tai on 12/12/16.
+//  Copyright © 2016 Brian Tai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
